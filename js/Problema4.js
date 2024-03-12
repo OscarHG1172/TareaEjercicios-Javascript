696356ee263ade37b950b4edb18220c5b3b0cc7a
¨Oscar <OscarLuisHG.117@gmail.com>
2024-03-11 23:09:41 -0600

    modified:   Problema5.html 	modified:   js/Problema4.js 	new file:   js/Problema5.js

--- js/Problema4.js
+++ js/Problema4.js
@@ -1,6 +1,9 @@
 let estudiante1Cursos =['Matemáticas', 'Inglés', 'Programación'];
 let estudiante2Cursos =['Geografía', 'Español', 'Programación'];
-if (estudiante1Cursos == estudiante2Cursos) {
-    
+for (let index = 0; index < estudiante1Cursos.length; index++) {
+    if (estudiante1Cursos==estudiante2Cursos) {
+        console.log('los estudiantes comparten 1 clase la cual es ');
+        
+    }
     
 }
---
+++ js/Problema5.js
@@ -0,0 +1,4 @@
+const gente = ["María", "Dani", "Luis", "Juan", "Camila"];
+console.log(gente[0]+' '+gente[1]+' '+gente[2]+' '+gente[3]+' '+gente[4]);
+console.log("Eliminado 1 elementos: " + gente.splice(1,1) );
+console.log(gente[0]+' '+gente[1]+' '+gente[3])
